5308f56b4a08e169f025ca1973b3297664249a84
Newton <17086936+livenet123@users.noreply.github.com>
2018-03-15 19:52:16 +0200

    Update config.js

--- website/config.js
+++ website/config.js
@@ -8,7 +8,7 @@ var email = "support@newtoncoin.tech";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
-var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+var easyminerDownload = "http://www.newtoncoin.tech/apps/windows/NewtonEasyMiner.zip";
 
 var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
 
